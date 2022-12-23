31a6970268c58489d73279908cd8b6020c29577a
Rickyslash <r.ckyslash@gmail.com>
2022-12-23 17:31:44 +0700

    add old finished work section 10

---
+++ Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/OLD (Finished)/Exception Handling Lessons/unchecked-exceptions/RuntimeException2.java
@@ -0,0 +1,10 @@
+public class RuntimeException2 {
+    public static void main(String[] args) {
+        String word = null;
+        if (word == null) {
+            System.out.println("The word is null");
+        } else {
+            System.out.println(word);
+        }
+    }
+}
