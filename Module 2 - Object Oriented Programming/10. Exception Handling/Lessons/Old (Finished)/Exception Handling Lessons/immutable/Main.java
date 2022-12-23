1f5b610d7d683f80ae338febb3299b3357f27e6e
Rickyslash <r.ckyslash@gmail.com>
2022-12-23 17:02:31 +0700

    Revert "old work (finished)"
    This reverts commit a8726e3a034124da4d2f00d9dd5b8f7a1fccc895.

--- Module 2 - Object Oriented Programming/10. Exception Handling/Lessons/Old (Finished)/Exception Handling Lessons/immutable/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person person = new Person("Ryan", "Canadian"); // (No: Mutable or) Immutable(?)
-        // doesn't have setters
-    }
-}
